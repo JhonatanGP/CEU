43dc05183ff493fb005a96ab41eeebad7ea88c63
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-01-31 16:24:53 +0100

    l

--- PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio41/App.java
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio41/App.java
@@ -1,10 +1,20 @@
 package ejercicio41;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Dime el nombre del primer equipo");
+		String 
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio41/Equipo.java
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio41/Equipo.java
@@ -7,6 +7,25 @@ public class Equipo {
 	private Jugador capitan;
 	private List<Jugador>jugadores;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Jugador getCapitan() {
+		return capitan;
+	}
+	public void setCapitan(Jugador capitan) {
+		this.capitan = capitan;
+	}
+	public List<Jugador> getJugadores() {
+		return jugadores;
+	}
+	public void setJugadores(List<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
+	
 	
 
 }
