e8c66d89633d2dfbe7968c45d1d56e765fac6f42
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-02-05 17:36:05 +0100

    j

--- PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio45/App.java
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio45/App.java
@@ -1,10 +1,29 @@
 package ejercicio45;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Boolean correcto = false;
+		LocalDate hoy = LocalDate.now();
+		LocalDate nacimiento;
+		Scanner scanner = new Scanner(System.in);
+		
+		do {
+			System.out.println("Introduce tu fecha de nacimiento");
+			String fecha = scanner.nextLine();
+			
+			Integer dia = Integer.parseInt(fecha.split("/")[0]);
+			Integer mes = Integer.parseInt(fecha.split("/")[1]);
+			Integer ano = Integer.parseInt(fecha.split("/")[2]);
+			
+			nacimiento = LocalDate.of(ano, mes, dia);
+			
 
+		}
+		
 	}
 
 }
