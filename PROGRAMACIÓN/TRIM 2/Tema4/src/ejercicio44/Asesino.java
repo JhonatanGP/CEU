4e82102fe35d3783ea4d9d3016f164b6cff0f085
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-02-05 16:48:25 +0100

    fg

---
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio44/Asesino.java
@@ -0,0 +1,25 @@
+package ejercicio44;
+
+public class Asesino extends Personaje {
+	
+	private static final Integer PUNTOS_VIDA = 100;
+	private static final Integer DAÑO = 10;
+	private static final Integer DAÑO_TIEMPO = 5;
+	
+	@Override
+	public Integer getPuntosVida() {
+		// TODO Auto-generated method stub
+		return PUNTOS_VIDA;
+	}
+	@Override
+	public Integer getDaño() {
+		// TODO Auto-generated method stub
+		return DAÑO;
+	}
+	@Override
+	public Integer getDañoTiempo() {
+		// TODO Auto-generated method stub
+		return DAÑO_TIEMPO;
+	}
+	
+}
