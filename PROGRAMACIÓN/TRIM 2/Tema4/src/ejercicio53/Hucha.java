fcecad62f9bbddbfbe491b35916ea7456068b047
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-02-16 18:42:15 +0100

    g

---
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio53/Hucha.java
@@ -0,0 +1,36 @@
+package ejercicio53;
+
+import java.math.BigDecimal;
+
+public class Hucha {
+
+	private BigDecimal dinero;
+	
+//Creo los métodos no puedo ponerlo static porque si no metería dinero en todos
+	public  BigDecimal meterDinero(BigDecimal dinero) {
+		return dinero;
+		
+		if (dinero < hucha) {
+			
+		}
+	}
+	
+	public  BigDecimal sacarDinero(BigDecimal dinero) {
+		return dinero;
+	}
+	
+	public  BigDecimal contarDinero(BigDecimal dinero) {
+		return dinero;
+	}
+	
+	public  BigDecimal romperHucha(BigDecimal dinero) {
+		return dinero;
+	}
+
+	@Override
+	public String toString() {
+		return "Hucha [dinero=" + dinero + "]";
+	}
+	
+		
+}
