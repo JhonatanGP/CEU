10b76626bacd8a5c6c69f8570fb4a4cc6244d457
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-03-20 16:37:26 +0100

    3

---
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio56/app.java
@@ -0,0 +1,28 @@
+package ejercicio56;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class app {
+
+	public static void main(String[] args) {
+		
+	
+		// TODO Auto-generated method stub
+		Map<Integer, Integer> num = new HashMap<Integer, Integer>();
+		int Map = 0;
+		if(Map  = 0) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Introduce un numero");
+			Integer num1 = scanner.nextInt();
+			
+		}
+		
+		
+
+
+	}
+
+}
