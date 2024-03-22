4c65600e7a7f8e0d47a8d5747d50f6e7cd8ab643
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-03-22 16:50:41 +0100

    e

--- PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio56/app.java
+++ PROGRAMACIÓN/TRIM 2/Tema4/src/ejercicio56/app.java
@@ -1,28 +1,58 @@
 package ejercicio56;
-
-import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
+
 
 public class app {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		Map<Integer, Integer> numeros = new HashMap<>();
+		
+		Integer numero;// = -1;
+		Integer contadorTotal = 0;
 	
-		// TODO Auto-generated method stub
-		Map<Integer, Integer> num = new HashMap<Integer, Integer>();
-		
-		if(Map) {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("Introduce un numero");
-			Integer num1 = scanner.nextInt();
+		do {
 			
+			System.out.println("Dame un número por favor");
+			numero = scanner.nextInt();
+			
+			if(!numero.equals(0)){
+				
+				if (numeros.containsKey(numero)) {
+					Integer cantidadActual = numeros.get(numero);
+					numeros.put(numero, cantidadActual + 1);
+				}
+				else {
+					numeros.put(numero, 1);
+				}
+				contadorTotal ++;				
+			}	
+		}while(!numero.equals(0));		
+		
+		scanner.close();
+		
+		imprimirNumeros(numeros, contadorTotal);
+	}
+
+	private static void imprimirNumeros(Map<Integer, Integer> numeros, Integer contadorTotal) {
+		System.out.println("Total números indicados " + contadorTotal);
+		System.out.println("Distribución: ");
+		Set<Entry<Integer, Integer>> entrys = numeros.entrySet();
+		for (Entry<Integer, Integer> entry : entrys) {
+			String terminacion = "veces";
+			if (entry.getValue().equals(1)) {
+				terminacion = "vez";
+			}
+			System.out.println("\t> Número " + entry.getKey() + ": " + entry.getValue() + " " + terminacion);
 		}
-		
-		
 
-
+		
 	}
 
 }
