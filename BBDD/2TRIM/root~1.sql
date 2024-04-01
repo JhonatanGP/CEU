112e5c7e2bd5b6791d626f188c9fa71ce98d6f7f
jhonatan <jguzman5292@ceuandalucia.es>
2024-04-01 13:08:37 +0200

    t

--- BBDD/2TRIM/root~1.sql
+++ BBDD/2TRIM/root~1.sql
@@ -3,3 +3,5 @@ create user TEMA6 identified by a123456
 default tablespace system 
 quota unlimited on system;
 grant connect, create session, create table TO TEMA6;
+grant create procedure to TEMA6;
+
