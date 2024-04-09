0324be7f45cc8695168ab48799dee7cecb7c438e
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-04-09 20:57:09 +0200

    e

--- PROGRAMACIÓN/TRIM 2/Tema6/src/ej01/servicios/PersonaService.java
+++ PROGRAMACIÓN/TRIM 2/Tema6/src/ej01/servicios/PersonaService.java
@@ -1,9 +1,10 @@
 package ej01.servicios;
 
-import java.beans.Statement;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import ej01.modelo.Persona;
 
@@ -24,6 +25,26 @@ public class PersonaService {
 		try {
 			
 			conn = openConn.getNewConnection();
+			stmt = conn.createStatement();
+			
+			String sql = "SELECT * FROM PERSONAS WHERE DNI = '" + dni + "'";
+			System.out.println(sql);
+			rs = stmt.executeQuery(sql);
+			
+			if(rs.next()) {
+				Persona p = new Persona();
+				p.setNombre(rs.getString("NOMBRE"));
+				p.setDni(rs.getString("DNI"));
+				
+				String apellido = rs.getString("APELLIDOS");
+				p.setApellidos(apellidos);
+				p.setFechaNac(rs.getDate("FECHA_NAC").toLocalDate());
+				//return getPersonaFromResultSet(rs);				
+			}else {
+				
+			}
+			
+		}catch(SQLException e){
 			
 		}
 		
