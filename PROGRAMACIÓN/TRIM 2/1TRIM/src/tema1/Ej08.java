5009c435bcd016d6e0afd688d05beda5a04be07e
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-04-12 20:31:25 +0200

    frg

--- PROGRAMACIÓN/TRIM 2/1TRIM/src/tema1/Ej08.java
+++ PROGRAMACIÓN/TRIM 2/1TRIM/src/tema1/Ej08.java
@@ -1,15 +1,19 @@
 package tema1;
 
+import java.util.Scanner;
+
 public class Ej08 {
 
 	public static void main(String[] args) {
 //Realizar un programa que obtenga por teclado los siguientes datos:
+		Scanner sc = new Scanner(System.in);
 	//Un número, pero registradlo en una variable de tipo String llamada a.
-		String a = "6";
+		System.out.println("Dame u");
+		String a = sc.nextLine();
 	//Un número decimal registrado en una variable de tipo Double llamada b.
-		Double b = 8.0;
+		Double b = sc.nextDouble();
 	//Un número entero registrado en una variable de tipo Float llamada c. 
-		
+		Float c = sc.nextFloat();
 //	A continuación, haz las siguientes conversiones de tipo:
 	//Guarda el valor de “a” en una variable de tipo Long llamada “x1”.
 		
@@ -20,6 +24,11 @@ public class Ej08 {
 	//Guarda el valor de “c” en una variable de tipo Long llamada “x4”.
 		
 	//Imprime por consola el valor de todas las variables x1, x2, x3 y x4. 
+		
+		
+		
+		
+		
 		
 	}
 
