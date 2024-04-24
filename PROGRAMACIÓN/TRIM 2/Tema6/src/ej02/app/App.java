36fcd09201121a6a2a6c2e9158f3799e78d44d01
jhonatan <jguzman5292@ceuandalucia.es>
2024-04-24 17:16:29 +0200

    f

--- PROGRAMACIÓN/TRIM 2/Tema6/src/ej02/app/App.java
+++ PROGRAMACIÓN/TRIM 2/Tema6/src/ej02/app/App.java
@@ -1,6 +1,7 @@
 package ej02.app;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 import ej01.modelo.Persona;
@@ -11,13 +12,14 @@ public class App {
 	public static void main(String[] args) {
 
 		PersonaService service = new PersonaService();
-		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Indica el DNI de la persona");
-		String vDNI = sc.nextLine();
-		
 		try {
+			// Por DNI
+			//consultarPersona(service, scanner);
+
+			System.out.println("Indica el DNI de la persona");
+			String vDNI = sc.nextLine();
 			Persona p = service.consultarPersona(vDNI);
 			
 			if (p == null) {
@@ -25,6 +27,22 @@ public class App {
 			} else {
 				System.out.println(p);
 			}
+			// Filtro para buscar por nombre o apellidos
+
+			//consultarPersonas(service, scanner);
+				System.out.println("Indica nombre o apellidos a buscar:");
+				String filtro = "";
+				do {
+					filtro = scanner.nextLine();
+				} while (filtro.isEmpty());
+
+				List<Persona> lista = service.consultarPersonas(filtro);
+				if(lista.isEmpty())
+						System.out.println("No hay ninguna persona con ese filtro");
+				for (Persona persona : lista) {
+							System.out.println(persona);
+						}
+
 		} catch (SQLException e) {
 			System.out.println("Exception en main: " + e.getMessage());
 		}finally {
