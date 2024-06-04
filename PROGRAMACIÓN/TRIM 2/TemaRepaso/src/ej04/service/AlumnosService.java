0e2e3b48e5c0ed544bd891325fbc51fd5b5fe782
JhonatanGP <jhonatanguzman19998@gmail.com>
2024-06-04 17:34:46 +0200

    k

--- PROGRAMACIÓN/TRIM 2/TemaRepaso/src/ej04/service/AlumnosService.java
+++ PROGRAMACIÓN/TRIM 2/TemaRepaso/src/ej04/service/AlumnosService.java
@@ -1,9 +1,29 @@
 package ej04.service;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import ej04.modelo.Alumno;
+
 public class AlumnosService {
 
+	private OpenConnection openConn;
+
 	public AlumnosService() {
-		// TODO Auto-generated constructor stub
+		openConn = new OpenConnection();
+	}
+	
+	public Alumno insertarAlumno(String dni) throws SQLException{
+	
+		String sql = "INSERT INTO ALUMNOS VALUES (?,?,?,?)";
+		 
+		try ( Connection conn = openConn.getNewConnection(); PreparedStament stmt = conn.prepareStatement(sql)){
+			
+		}
+		
+		
 	}
 
 }
